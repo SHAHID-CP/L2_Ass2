0d277a1aefd52d043716aa10dd785548512ee4d0
Black Fighter <blackfighter@gmail.com>
2026-06-03 12:28:58 +0600

    some modify

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,30 @@
+import helmet from "helmet";
+import cors from "cors";
 import express, {type Application,type Request,type Response} from "express";
 import CookieParser from "cookie-parser";
-import cors from "cors";
+
 import { authRoutes } from "./modules/auth/auth.route";
 import { issueRoutes } from "./modules/issues/issues.route";
 import globalErrorHandler from "./middleware/globalErrorHandeler";
 
 
+
 const app: Application = express();
 
+app.use(helmet());
+app.use(cors({origin: "http://localhost:3000",}),);
 app.use(CookieParser());
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  }),
-);
+
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success:true,
-    message: "Wellcome to PostgreSql Express Server"
+    message: "Wellcome to Dev pulse Server"
   });
 });
 
