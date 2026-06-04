99a5968e210083fe2119557f141d2dd4c12cbc79
Black Fighter <blackfighter@gmail.com>
2026-06-04 09:24:42 +0600

    tsup set

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/server.ts'],
+  outDir: 'dist',
+  format: ['cjs'],
+  target: 'esnext',
+  platform: 'node',
+  bundle: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+
+  banner: {
+    js: `
+      import { createRequire } from 'module';
+      const require = createRequire(import.meta.url);
+    `,
+  },
+});
